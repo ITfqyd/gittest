d54b753f33085e67715e6059970423515f3d875d
ITfqyd <myworked@163.com>
2020-02-05 23:22:33 +0800

    程序猿学社第一次把代码提交到本地库中

---
+++ src/main/java/com/cxyxs/gittest/test/Test.java
@@ -0,0 +1,14 @@
+package com.cxyxs.gittest.test;
+
+/**
+ * Description：转发请注明来源  程序猿学社 - https://ithub.blog.csdn.net/
+ * Author: 程序猿学社
+ * Date:  2020/2/5 22:18
+ * Modified By:
+ */
+public class Test {
+    public static void main(String[] args) {
+        String str="程序猿学社说武汉加油!";
+        System.out.println(str);
+    }
+}
